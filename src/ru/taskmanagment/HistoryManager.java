77535b57d6f6e7eca193459490010f722d8a8f85
klv2503 <168983480+klv2503@users.noreply.github.com>
2024-06-22 11:01:49 +0300

    Update HistoryManager.java согласно pull checking

--- src/ru/taskmanagment/HistoryManager.java
+++ src/ru/taskmanagment/HistoryManager.java
@@ -1,10 +1,11 @@
 package ru.taskmanagment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface HistoryManager {
     void add(String id, Task task);
+
     void remove(String id);
+    
     List<Task> getHistory();
 }
