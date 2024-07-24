0ec4c385fb66ace9387695cc6f532172540357da
Лев Курас <levkuras@yandex.kz>
2024-07-24 06:58:53 +0200

    Выполнение ТЗ к Спринту-8. Добавлена работа со временем. Циклы forEach переписаны в Stream. Выявлены ошибки в понимании предыдущих ТЗ, соответствующие исправления внесены в код. Добавлено несколько методов и классов с целью улучшения структуры кода.

---
+++ src/ru/taskmanagment/CSVFormatter.java
@@ -0,0 +1,35 @@
+package ru.taskmanagment;
+
+public class CSVFormatter {
+
+    private CSVFormatter() {
+
+    }
+
+    public static String convertDataToCSVString(Task task) {
+        return task.taskToCSV();
+    }
+
+    public static String convertDataToCSVString(Task task, int epicId) {
+        String str = task.taskToCSV();
+        int ind = str.indexOf("\n");
+        return str.substring(0, ind - 1) + "," + epicId + "\n";
+    }
+
+    public static Task fromCSV2Task(String value) {
+        String[] toTask = value.split(",");
+        switch (toTask[1]) {
+            case "TASK":
+                return new Task(Integer.parseInt(toTask[0]), toTask[2], toTask[4]);
+            case "EPIC":
+                return new Epic(Integer.parseInt(toTask[0]), toTask[2], toTask[4], Status.valueOf(toTask[3]),
+                        toTask[5], toTask[6], toTask[7]);
+            case "SUBTASK":
+                return new SubTask(Integer.parseInt(toTask[0]), toTask[2], Status.valueOf(toTask[3]), toTask[4],
+                        toTask[5], toTask[6], Integer.parseInt(toTask[8]));
+            default:
+                return null;
+        }
+    }
+
+}
